4e5b46c54d34d6efc53970675270a9aaf2b74454
User Charkes <charkes.hanna@gmail.com>
2026-01-01 22:48:54 +0300

    added createOrderWithValidIngredientsTest

---
+++ src/main/java/pojo/request/CreateOrderRequest.java
@@ -0,0 +1,16 @@
+package pojo.request;
+
+import java.util.List;
+
+public class CreateOrderRequest {
+    private List<String> ingredients;
+
+    public CreateOrderRequest() {}
+    public CreateOrderRequest(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+}
---
+++ src/test/java/steps/OrdersSteps.java
@@ -0,0 +1,72 @@
+package steps;
+
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import pojo.request.CreateOrderRequest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OrdersSteps {
+
+    @Step("Получим список всех ингредиентов")
+    //получим список ингредиентов
+    public List<String> getAllIngredientsList() {
+        return given()
+                .when()
+                .get("/api/ingredients")
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getList("data._id", String.class);
+    }
+
+    @Step("Method to choose random ingredients")
+    public List<String> getRandomIngredients(List<String> allIngredients, int count) {
+        List<String> copy = new ArrayList<>(allIngredients);
+        Collections.shuffle(copy);
+        return copy.subList(0, count);
+    }
+
+    @Step("Отправим запрос с заказом")
+    public Response sendCreateOrderRequest(String accessToken, CreateOrderRequest createOrderRequest) {
+        return given()
+                .log().all()
+                .header("Content-type", "application/json")
+                .header("Authorization", accessToken)
+                .body(createOrderRequest)
+                .when()
+                .post("/api/orders");
+    }
+
+    @Step("Отправим заказ без токена")
+    public Response sendCreateOrderRequestWithoutToken(CreateOrderRequest createOrderRequest) {
+        return given()
+                .log().all()
+                .header("Content-type", "application/json")
+                .body(createOrderRequest)
+                .when()
+                .post("/api/orders");
+    }
+
+    @Step("Check status code {expectedCode} of the response")
+    public  void checkStatusCode(Response response, int expectedCode) {
+        int actualCode = response.getStatusCode();
+        assertEquals(expectedCode, actualCode, "Статус-код не совпадает!");
+    }
+
+    @Step("Check message body: value '{key}' equals '{expectedValue}'")
+    public  void checkResponseValue(Response response, String key, Object expectedValue) {
+        Object actualValue = response.jsonPath().get(key);
+        assertEquals(expectedValue, actualValue, "Значение по ключу '" + key + "' не совпадает");
+    }
+
+
+
+}
